95189af8d12b4cebf5f0e7ce03782e4726a7622e
Kristin Evans <kevans0625@gmail.com>
2020-04-19 04:11:58 -0500

    created a connection the the mongoosedb

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -8,6 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+//set up mongoose db
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
 
 app.listen(3000, () => {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
