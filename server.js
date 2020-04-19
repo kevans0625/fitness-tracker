ad7985087882a74e5d95a2eaecf81c7f186772f3
Kristin Evans <kevans0625@gmail.com>
2020-04-19 04:06:28 -0500

    added  in middleware

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
 
 app.listen(3000, () => {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
