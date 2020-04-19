7da15d583f5ab36f504311da8ee592377ac91289
Kristin Evans <kevans0625@gmail.com>
2020-04-19 04:12:49 -0500

    created a connection the the mongoosedb

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //set up mongoose db
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", { useNewUrlParser: true });
 
 app.listen(3000, () => {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
